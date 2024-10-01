7c910e23652a3c16d526af660e455ab8a97d5eae
rutvisutariya10 <rutvisutariya10@gmail.com>
2024-09-30 21:00:33 -0400

    Time: 4125 ms (53.66%), Space: 16.7 MB (6.13%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,26 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS,COLS = len(board),len(board[0])
-        path = set()
-        def dfs(row,col,i):
-            if i == len(word):
-                return True
-            if row <0 or col <0 or row >= ROWS or col >= COLS or word[i] != board[row][col] or (row,col) in path:
-                return False
-            path.add((row,col))
-            res = (dfs(row + 1,col,i+1) or
-                  dfs(row - 1,col,i+1) or
-                  dfs(row,col + 1,i+1) or
-                  dfs(row,col - 1,i+1))
-            path.remove((row,col))
-            return res
         
+        visited = set()
+        
+        def bfs(row,col,i):
+            if i == len(word) or row < 0 or col < 0 or row >= ROWS or col >= COLS or board[row][col] != word[i] or (row,col) in visited:
+                return False
+            if i == len(word) - 1:
+                return True                
+            visited.add((row,col))
+            res = bfs(row - 1, col, i + 1) or bfs(row + 1, col, i + 1) or bfs(row, col - 1, i + 1) or bfs(row, col + 1, i + 1)
+            visited.remove((row,col))
+            return res
+            
+            
+            
         for i in range(ROWS):
             for j in range(COLS):
-                if dfs(i,j,0): return True
+                if bfs(i,j,0): return True
         
         return False
+        
+                
         
