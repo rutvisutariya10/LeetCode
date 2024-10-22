d05f54c67ea1a8112f6c7d396eddaead4e448730
rutvisutariya10 <rutvisutariya10@gmail.com>
2024-10-22 18:30:22 -0400

    Time: 250 ms (50.05%), Space: 27.9 MB (10.56%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,38 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        number = 0
-        ROWS, COLS = len(grid), len(grid[0])
-        path = set()
+        M,N = len(grid),len(grid[0])
+        count = 0
+        visited = set()
+        def findIsland(r,c):
+            if r < 0 or r >= M or c < 0 or c >= N or (r,c) in visited or grid[r][c] == "0":
+                return 
+            visited.add((r,c))
+            findIsland(r-1,c)
+            findIsland(r+1,c)
+            findIsland(r,c-1)
+            findIsland(r,c+1)
         
-        # def dfs(r,c):
-        #     if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == "0" or (r,c) in path:
-        #         return
-        #     path.add((r,c))
-        #     dfs(r-1,c)
-        #     dfs(r+1,c)
-        #     dfs(r,c-1)
-        #     dfs(r,c+1)
+        for i in range(M):
+            for j in range(N):
+                if (i,j) not in visited and grid[i][j] == "1":
+                    count += 1
+                    findIsland(i,j)
         
-        def bfs(r,c):
-            
-            queue = collections.deque()
-            path.add((r,c))
-            queue.append((r,c))
-            while queue:
-                R,C = queue.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-                for dr,dc in directions:
-                    x = R + dr
-                    y = C + dc
-                    if x >= 0 and x < ROWS and y >= 0 and y < COLS and (x,y) not in path and grid[x][y] == "1":
-                        path.add((x,y))
-                        queue.append((x,y))
-            
-            
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == "1" and (r,c) not in path:
-                    number += 1
-                    bfs(r,c)
-        return number
+        return count
+        
         
