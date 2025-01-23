bc13bc799505fb0c85d5bffd3f1cc9a7485138d0
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-01-23 14:26:32 -0500

    Update file 1/23/2025, 2:26:33 PM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,17 +1,17 @@
-            if r < 0 or c < 0 or r >= M or c >= N or (r,c) in visited or board[r][c] == "X":
-                return True
-            if board[r][c] == "O" and (r == 0 or c == 0 or r == M - 1 or c == N - 1):
-                return False
-            visited.add((r,c))
             pathVisited.add((r,c))
             return dfs(r-1,c) and dfs(r+1,c) and dfs(r,c-1) and dfs(r,c+1)
         
-        def dfs(r,c):
-        M, N = len(board), len(board[0])
-        visited, pathVisited = set(),set()
-        Do not return anything, modify board in-place instead.
-        """
-        """
-    def solve(self, board: List[List[str]]) -> None:
         for row in range(M):
             for col in range(N):
+                if board[row][col] == "O" and (row,col) not in visited:
+                    pathVisited = set()
+            visited.add((r,c))
+                return False
+            if board[r][c] == "O" and (r == 0 or c == 0 or r == M - 1 or c == N - 1):
+                return True
+            if r < 0 or c < 0 or r >= M or c >= N or (r,c) in pathVisited or board[r][c] == "X":
+        def dfs(r,c):
+        visited, pathVisited = set(),set()
+        M, N = len(board), len(board[0])
+        """
+        Do not return anything, modify board in-place instead.
