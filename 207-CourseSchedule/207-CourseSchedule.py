b987deec67074c9fcb1e1a42f53ed71f1b9d29cc
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-01-23 15:00:47 -0500

    Create file

---
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -0,0 +1,17 @@
+        for i in range(numCourses):
+
+            
+            for nei in preq[i]:
+                    if not dfs(nei):
+                        return False
+            return True
+            visited.add(i)
+                return False
+            if i in visited:
+        def dfs(i):
+        completed = set()
+            preq[a].append(b)
+            completed.add(i)
+            visited = set()
+            if not dfs(i):
+                if nei not in completed:
