e01723e696c2938f1ddda75ec2431d4a70dc3a90
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-01-23 14:25:32 -0500

    Update file 1/23/2025, 2:25:33 PM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,17 +1,17 @@
-        for i in range(M):
-            dfs(r,c-1)
-            dfs(r,c+1)
-            dfs(r+1,c)
-            dfs(r-1,c)
-            unsurrounded.add((r,c))
-                return
-            if r < 0 or c < 0 or r >= M or c >= N or (r,c) in unsurrounded or board[r][c] == "X":
+            if r < 0 or c < 0 or r >= M or c >= N or (r,c) in visited or board[r][c] == "X":
+                return True
+            if board[r][c] == "O" and (r == 0 or c == 0 or r == M - 1 or c == N - 1):
+                return False
+            visited.add((r,c))
+            pathVisited.add((r,c))
+            return dfs(r-1,c) and dfs(r+1,c) and dfs(r,c-1) and dfs(r,c+1)
+        
         def dfs(r,c):
-        unsurrounded = set()
         M, N = len(board), len(board[0])
-        """
+        visited, pathVisited = set(),set()
         Do not return anything, modify board in-place instead.
         """
-            print(r,c)
-            dfs(i,0)
-            dfs(i,N-1)
+        """
+    def solve(self, board: List[List[str]]) -> None:
+        for row in range(M):
+            for col in range(N):
