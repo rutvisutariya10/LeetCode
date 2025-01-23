1aaba1eaedf5246c938756f85d71beac0f701776
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-01-23 18:26:48 -0500

    Create file

---
+++ 332-ReconstructItinerary/332-ReconstructItinerary.py
@@ -0,0 +1,16 @@
+            while graph[curr]:
+            answer.append(curr)
+        def dfs(curr):
+        answer = []
+            graph[fro].append(to)
+        for fro,to in tickets:
+        tickets.sort(reverse=True)
+        graph = collections.defaultdict(list)
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+class Solution:
+                nei = graph[curr].pop()
+                dfs(nei)
+        dfs("JFK")
+        return answer[::-1]
+
+
