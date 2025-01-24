f782db5fe64d051acf5225a7db93ad05586352f8
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-01-24 16:21:05 -0500

    Update file 1/24/2025, 4:21:06 PM

--- 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
+++ 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
@@ -1,16 +1,17 @@
-        def find(tupl):
-    
-            rank[(x,y)] = 0
-            parent[(x,y)] = (x,y)
-        for x,y in points:
-        rank = {}
-        parent = {}
-                heapq.heappush(dist,[val,xi,yi,xj,yj])
-                val = abs(xi-xj) + abs(yi-yj)
-                xi, yi = points[i][0], points[i][1]
-                xj, yj = points[j][0], points[j][1]
-            for j in range(i+1,len(points)):
-        for i in range(len(points)):
-        dist = []
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-class Solution:
+                g[i].append([val,j])
+                g[j].append([val,i])
+        
+        pq = [[0,0]]
+        visited = set()
+        while len(visited) < len(points):
+            cost, i = heapq.heappop(pq)
+            if i in visited:
+                continue
+        answer = 0
+            visited.add(i)
+            answer += cost
+            for nei in g[i]:
+                heapq.heappush(pq,nei)
+        return answer
+
+        print(g)
