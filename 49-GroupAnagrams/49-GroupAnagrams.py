924dea486e2982341f40b915dabf13a31ad93e65
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-10 13:19:53 -0500

    Update file 2/10/2025, 1:19:53 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,20 +1,19 @@
-                return False
-            if sum(x.values()) != sum(y.values()):
-            for i in x.keys():
-                if i not in y or x[i] != y[i]:
-                    return False
             return True
         used = set()
         answer = []
         for i in range(len(strs)):
-            if strs[i] in used:
+            if i in used:
                 continue
             ana = [strs[i]]
             for j in range(i+1,len(strs)):
-                if isAnagram(ht[strs[i]],ht[strs[j]]) and strs[j] not in used:
+                if isAnagram(ht[i],ht[j]) and j not in used:
                     ana.append(strs[j])
-                    used.add(strs[j])
+                    used.add(j)
             answer.append(ana)
+            used.add(i)
         return answer
                 
 
+
+
+        
