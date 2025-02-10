90e42321e496276725aac6e2515ddea70b80660c
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-10 13:28:34 -0500

    Update file 2/10/2025, 1:28:35 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,20 +1,12 @@
-            if i in used:
-                continue
-            ana = [strs[i]]
-            used.add(i)
-        for i in range(len(strs)):
-        answer = []
-        used = set()
-            ht.append(h)
-                h[c] = h.get(c, 0) + 1
-            for c in s:
-            h = {}
-            for j in range(i+1,len(strs)):
-                if ht[i] == ht[j] and j not in used:
-                    ana.append(strs[j])
-                    used.add(j)
-            answer.append(ana)
-        return answer
+        ''' 
+        Sort each word and add it to the dictionary corresponding to the sorted word
+        '''
+        ht = collections.defaultdict(list)
+        for s in strs:
+            ht[''.join(sorted(s))].append(s)
+        return list(ht.values())
                 
 
 
+
+        
