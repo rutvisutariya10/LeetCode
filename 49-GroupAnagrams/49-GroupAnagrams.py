426b8e5d05fc433a888f5b71a1de91178c34b441
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-10 13:21:59 -0500

    Update file 2/10/2025, 1:21:59 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,19 +1,20 @@
-            return True
-        used = set()
-        answer = []
-        for i in range(len(strs)):
             if i in used:
                 continue
             ana = [strs[i]]
+            used.add(i)
+        for i in range(len(strs)):
+        answer = []
+        used = set()
+            ht.append(h)
+                h[c] = h.get(c, 0) + 1
+            for c in s:
+            h = {}
             for j in range(i+1,len(strs)):
-                if isAnagram(ht[i],ht[j]) and j not in used:
+                if ht[i] == ht[j] and j not in used:
                     ana.append(strs[j])
                     used.add(j)
             answer.append(ana)
-            used.add(i)
         return answer
                 
 
 
-
-        
