cc1974f86fdbdbf96a044688f925e959f8b131a8
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-11 11:46:21 -0500

    Update file 2/11/2025, 11:46:22 AM

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,20 +1,23 @@
-            if height[l] <= height[r]:
-                currStored = 0
-                s = l
-                while l < r:
-                    currStored += height[s] - height[l]
-                    l += 1
-                waterStored += currStored 
-            r += 1
+        #         waterStored += currStored 
+        #     r += 1
+        # left = l
+        # l,r = H - 1, H - 1
+        # return waterStored
+
+        l, r = 0, len(height) - 1
+        l_max, r_max = height[l], height[r]
         waterStored = 0
-        while r < H:
-            r += 1
-        left = l
-        l,r = H - 1, H - 1
+        while l < r:
+            if l_max < r_max:
+                l += 1
+                waterStored += l_max - height[l]
+            else:
+                r -= 1
+                waterStored += r_max - height[r]
         return waterStored
 
 
             
+                l_max = max(height[l],l_max)
              
-
-        
+                r_max = max(height[r],r_max)
