36cc4c02df978e0ba6e5182fa5730691beb89688
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-13 12:45:31 -0500

    Create file

---
+++ 143-ReorderList/143-ReorderList.py
@@ -0,0 +1,15 @@
+        """
+        # fast,slow = head, head
+        # while fast and fast.next:
+        #     fast = fast.next.next
+        [1,2]
+        [4,3]
+        Do not return anything, modify head in-place instead.
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+class Solution:
+#         self.next = next
+#         self.val = val
+#     def __init__(self, val=0, next=None):
+# class ListNode:
+# Definition for singly-linked list.
