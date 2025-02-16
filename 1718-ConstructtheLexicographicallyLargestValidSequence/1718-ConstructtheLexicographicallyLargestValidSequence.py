e566f8489e3d51ad7c6f0b1bd33cf76f2fd45a5e
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-16 16:35:54 -0500

    Create file

---
+++ 1718-ConstructtheLexicographicallyLargestValidSequence/1718-ConstructtheLexicographicallyLargestValidSequence.py
@@ -0,0 +1,9 @@
+    def constructDistancedSequence(self, n: int) -> List[int]:
+    '''
+    [6,4,1,5,3,4,6,3,5]
+    [5,3,1,4,3,5,2,4,2]
+    [4,2,3,2,4,3,1]
+    [3,1,2,3,2]
+    [2,1,2]
+    '''
+class Solution:
