a4870683be8f5246b2608a7082a9aaf1b1ec23b1
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-17 11:49:57 -0500

    Create file

---
+++ 1079-LetterTilePossibilities/1079-LetterTilePossibilities.py
@@ -0,0 +1,17 @@
+                    dfs(idx+1,st+tile,dic)
+                    dic[tile] += 1
+            return
+
+            
+            
+
+
+        dfs(-1,"",count)
+        return len(answer)
+
+                    dic[tile] -= 1
+                if dic[tile] > 0:
+            for tile in tiles:
+            dfs(idx+1,st,dic)
+                return
+            if idx == L:
