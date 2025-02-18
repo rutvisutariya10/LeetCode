b1069443613f6091434052bab7bf4de8f05b40bd
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-18 08:22:27 -0500

    Update file 2/18/2025, 8:22:28 AM

--- 146-LRUCache/146-LRUCache.py
+++ 146-LRUCache/146-LRUCache.py
@@ -1,21 +1,10 @@
-        for k,v in self.queue:
-
-    def put(self, key: int, value: int) -> None:
-        for k,v in self.queue:
-            if k == key:
-                return v
-        return -1
-                self.queue.remove((k,v))
-                self.queue.append((k,v))
-            if k == key:
-                self.queue.remove((k,v))
-        self.queue.append((key,value))
-        while len(self.queue) > self.cap:
     def get(self, key: int) -> int:
 
-        
-            self.queue.popleft()
-        
+        node.next.prev = node.prev        
+        node.prev.next = node.next
+    def remove(self, node: ListNode) -> None:
+        self.newest.prev,node.next = node,self.newest
+        self.newest.prev.next, node.prev = node, self.newest.prev
+    def insert(self,node: ListNode) -> None:
 
-
-                break
+        self.newest.prev = self.oldest
