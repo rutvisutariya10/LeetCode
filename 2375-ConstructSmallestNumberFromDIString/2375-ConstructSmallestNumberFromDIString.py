db76f77291b64d99ff4a20fa0db0d92a2926a753
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-18 07:50:12 -0500

    Update file 2/18/2025, 7:50:13 AM

--- 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.py
+++ 2375-ConstructSmallestNumberFromDIString/2375-ConstructSmallestNumberFromDIString.py
@@ -1,10 +1,16 @@
                 if idx > 0:
+                    continue
+                if i in used:
+        def dfs(idx,st):
+            for i in range(1,L+1):
+        L = len(pattern) + 1
+        answer = []
+        used = set()
+            if idx == L:
+                answer.append(st)
+                return True
                     if (pattern[idx-1] == 'I' and i < int(st[-1])) or (pattern[idx-1] == 'D' and i > int(st[-1])):
                         continue
-                if idx == L - 1:
-                    if (pattern[idx-1] == 'I' and i > int(st[-1])) or (pattern[idx-1] == 'D' and i < int(st[-1])):
-                        answer.append(st + str(i))
-                        return True
                 used.add(i)
                 if dfs(idx+1,st+str(i)):
                     return True
@@ -15,7 +21,3 @@
         return answer[0]
 
 
-            
-            
-
-                    continue
