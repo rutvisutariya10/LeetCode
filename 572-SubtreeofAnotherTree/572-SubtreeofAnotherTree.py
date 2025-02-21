3886b3de0fa90d9155512beae994ed23d684f5fd
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-02-21 09:05:55 -0500

    Create file

---
+++ 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
@@ -0,0 +1,17 @@
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if (p and not q) or (q and not p):
+            return False
+        return (p.val == q.val) and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+
+        return self.isSameTree(root,subRoot) or self.isSameTree(root.left,subRoot) or self.isSameTree(root.right,subRoot)
+        
+#         self.val = val
+#     def __init__(self, val=0, left=None, right=None):
