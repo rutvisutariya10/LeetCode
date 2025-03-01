b97e0a5c9a380970c28a6c5e98f8203ddb8822d1
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-03-01 17:16:46 -0500

    Update file 3/1/2025, 5:16:47 PM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,6 +1,3 @@
-        S(2) = 1 + 1, 2
-        S(3) = 1 + 1 + 1, 1 + 2, 2 + 1 = 3
-        S(4) = 1 + 1 + 1 + 1, 1 + 1 + 2, 1 + 2 + 1, 2 + 1 + 1, 2 + 2
 
         Each time you can add either 1 or 2
         S(i) = ith stair,  1 <= i <= n
@@ -12,7 +9,7 @@
         answer = 0
         for i in range(3,n+1):
             answer = prev + before_that
-            begore_that, prev = prev, answer
+            before_that, prev = prev, answer
         if n == 1:
             return 1
         if n == 2:
