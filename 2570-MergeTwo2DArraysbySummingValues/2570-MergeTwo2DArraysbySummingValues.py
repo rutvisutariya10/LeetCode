e9cf5c0341b40c0e7bc4d77e11759dc0ba77d7d3
Rutvi Sutariya <rutvisutariya10@gmail.com>
2025-03-02 13:39:02 -0500

    Update file 3/2/2025, 1:39:02 PM

--- 2570-MergeTwo2DArraysbySummingValues/2570-MergeTwo2DArraysbySummingValues.py
+++ 2570-MergeTwo2DArraysbySummingValues/2570-MergeTwo2DArraysbySummingValues.py
@@ -1,11 +1,20 @@
-class Solution:
-    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        m, n = 0, 0
-        M, N = len(nums1), len(nums2)
-        result = []
-        while m < M and n < N:
-            key1, val1 = nums1[m]
-            key2, val2 = nums2[n]
-            if  key1 < key2:
                 result.append([key1, val1])
                 m += 1
+            elif key1 > key2:
+                result.append([key2,val2])
+                n += 1
+            else:
+                result.append([key1, val1+val2])
+                m += 1
+                n += 1
+        while m < M:
+            key1, val1 = nums1[m]
+            result.append([key1, val1])
+            m += 1
+        while n < N:
+            key2, val2 = nums2[n]
+            result.append([key2,val2])
+            n += 1
+        return result
+        
+        
